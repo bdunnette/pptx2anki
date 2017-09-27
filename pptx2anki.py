bb99516eb10e6e7268333e97df05c5d12485f522
Brian Dunnette <dunn0172@umn.edu>
2017-09-27 15:18:17 -0500

    fixing logic when next slide has no image; commenting & tidying

--- pptx2anki.py
+++ pptx2anki.py
@@ -33,47 +33,38 @@ prs = Presentation(sys.argv[1])
 print(prs)
 print(len(prs.slides))
 slide_info = [dict() for i in range(len(prs.slides))]
+# Loop through slides, adding them to slide_info dictionary, so that we can look ahead to next slide
 for slide_index, slide in enumerate(prs.slides):
-    # print("\n\n")
-    # print(slide_index)
-    # print(prs.slides[slide_index+1])
-    # print(slide)
-    # print(slide.notes_slide)
     note_text = ""
+    # Compile all text on this slide's notes_slide
     for ns in slide.notes_slide.shapes:
         if not str(ns.text).isnumeric():
             note_text += str(ns.text)
-    # print(note_text)
     slide_info[slide_index]['text'] = note_text.strip()
-    # print(slide.shapes)
+    # Extract image files so that we can import them into our Anki package
     for shape in slide.shapes:
-        # print(shape)
-        # print(shape.shape_type)
         if shape.shape_type == MSO_SHAPE_TYPE.PICTURE:
-            # print(shape.image)
-            # print(shape.image.filename)
-            # print(shape.image.size)
-            # print(shape.image.ext)
-            # print(shape.image.content_type)
-            # print(shape.image.blob)
             image_file = "%s.%s" % (slide_index, shape.image.ext)
             with open(image_file, 'wb') as f:
                 f.write(shape.image.blob)
             slide_info[slide_index]['image'] = image_file
 
-# print(slide_info)
+# Loop through slides in slide_info
 for idx, slide in enumerate(slide_info):
     if 'image' in slide and not 'processed' in slide:
         print(slide)
         next_slide = slide_info[idx+1]
         print(slide['text'] == next_slide['text'])
         print(slide['text'])
-        if slide['text'] == next_slide['text']:
+        # If next slide has same note text as this one, add that slide's image as the "back" of our card
+        if (slide['text'] == next_slide['text']) and ('image' in next_slide):
             back_text = "<img src='%s'><p>%s</p>" % (next_slide['image'],slide['text'])
             my_package.media_files.append(next_slide['image'])
             slide_info[idx+1]['processed'] = True
+        # Otherwise, just put the note text on the card back
         else:
             back_text = slide['text']
+        # Add note to deck
         my_note = genanki.Note(
           model=my_model,
           sort_field=idx,
@@ -81,4 +72,5 @@ for idx, slide in enumerate(slide_info):
         my_deck.add_note(my_note)
         my_package.media_files.append(slide['image'])
 
+# Export deck to .apkg file
 my_package.write_to_file('%s.apkg' % deck_name)
